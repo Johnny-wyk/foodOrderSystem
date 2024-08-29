3d39dd71afabe19963fcf6478abf247a81d083cc
Johnny <525860438@qq.com>
2024-08-29 17:33:26 +0800

    8.29

--- sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -1,16 +1,19 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetMealService;
+import com.sky.vo.DishVO;
+import com.sky.vo.SetmealVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/setmeal")
@@ -30,6 +33,65 @@ public class SetMealController {
     public Result save(@RequestBody SetmealDTO setmealDTO){
         log.info("新增菜品,{}",setmealDTO);
         setMealService.save(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("套餐分页查询")
+    public Result<PageResult> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO){
+        log.info("分页查询,{}",setmealPageQueryDTO);
+        PageResult pageResult=setMealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    @DeleteMapping
+    @ApiOperation("删除菜品")
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("删除菜品,{}",ids);
+        setMealService.delete(ids);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询套餐，用于修改页面回显数据
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询套餐")
+    public Result queryById(@PathVariable Long id){
+        SetmealVO setmealVO=setMealService.queryById(id);
+        return Result.success(setmealVO);
+    }
+
+    /**
+     * 修改套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改套餐")
+    public Result update(@RequestBody SetmealDTO setmealDTO){
+        setMealService.update(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 套餐起售停售
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("套餐起售停售")
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        setMealService.startOrStop(status, id);
         return Result.success();
     }
 }
